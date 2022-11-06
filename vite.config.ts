a320d9fab4884345d2c90a5c34cf1efc202018ed
jiangyuhang <jiangyuhang@enn.cn>
2022-11-06 17:32:09 +0800

    新建一个monorepo的项目

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom' // 或 'happy-dom', 'node'
+    environment: 'jsdom',// 或 'happy-dom', 'node'
+
   }
 })
